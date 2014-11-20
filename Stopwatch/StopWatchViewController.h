2a68ac76e7c62126066c95358f46d834374ec697
Daniel Loffgren <ryukojiro@gmail.com>
2014-11-19 17:51:15 -0800

    Setup stopwatch MVC

---
+++ Stopwatch/StopWatchViewController.h
@@ -0,0 +1,15 @@
+//
+//  StopWatchViewController.h
+//  Stopwatch
+//
+//  Created by Daniel Loffgren on 11/19/14.
+//  Copyright (c) 2014 Daniel Loffgren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StopWatchViewController : UIViewController
+
+
+@end
+
---
+++ Stopwatch/StopWatchViewController.m
@@ -0,0 +1,27 @@
+//
+//  StopWatchViewController.m
+//  Stopwatch
+//
+//  Created by Daniel Loffgren on 11/19/14.
+//  Copyright (c) 2014 Daniel Loffgren. All rights reserved.
+//
+
+#import "StopWatchViewController.h"
+
+@interface StopWatchViewController ()
+
+@end
+
+@implementation StopWatchViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
